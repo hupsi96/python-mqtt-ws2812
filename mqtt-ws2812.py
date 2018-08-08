be99a8b6b3d6fe009d7301cff05e1679ecebfce0
Christian Huppertz <christian.huppertz@tum.de>
2018-08-08 19:04:39 +0200

    test 2

--- mqtt-ws2812.py
+++ mqtt-ws2812.py
@@ -2,7 +2,7 @@
 import paho.mqtt.client as mqtt
 from neopixel import *
 
-
+strip = Adafruit_NeoPixel(16, 18, 800000, 5, False, 255)
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
@@ -10,6 +10,9 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
+    strip.begin()
+    strip.setPixelColorRGB(1, 255, 255, 255)
+    strip.show()
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -19,7 +22,4 @@ client.connect("192.168.2.114", 1883, 60)
 
 #client.loop_forever()
 
-strip = Adafruit_NeoPixel(16, 18, 800000, 5, False, 255)
-strip.begin()
-strip.setPixelColorRGB(1, 255, 255, 255)
-strip.show()
+
