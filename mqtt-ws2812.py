22e5f7cdd380b50660b37e404a830af4643e5162
Christian Huppertz <christian.huppertz@tum.de>
2018-08-08 19:02:08 +0200

    add first try for LED in python

--- mqtt-ws2812.py
+++ mqtt-ws2812.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import paho.mqtt.client as mqtt
+from neopixel import *
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -16,3 +18,8 @@ client.on_message = on_message
 client.connect("192.168.2.114", 1883, 60)
 
 client.loop_forever()
+
+strip = Adafruit_NeoPixel(16, 18, 800000, 5, False, 255)
+strip.begin()
+strip.setPixelColorRGB(1, 255, 255, 255)
+strip.show()
