eae3f54c0891f6e9658157c2b01ba9cbd3acf0a2
Christian Huppertz <christian.huppertz@tum.de>
2018-08-08 22:59:29 +0200

    split up into multible functions

--- mqtt-ws2812.py
+++ mqtt-ws2812.py
@@ -6,42 +6,50 @@ import time
 
 
 strip = Adafruit_NeoPixel(16, 18, 800000, 5, False, 255)
+strip.begin()
+currrentBrightness = np.uint8(strip.getBrightness())
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
     client.subscribe("zimmer/#")
-    strip.begin()
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
 
+#Brightness
     if msg.topic == "zimmer/map/brightness/set":
         currrentBrightness = np.uint8(strip.getBrightness())
-        dif = int(msg.payload) - currrentBrightness
-        if dif > 0:
-            for x in range(1,dif+1):
-                strip.setBrightness(currrentBrightness + x)
-                strip.show
-                print(str(currrentBrightness + x))
-                time.sleep(.030)
-        elif dif < 0:
-            dif = dif * (-1)
-            for x in range(1,dif+1):
-                strip.setBrightness(currrentBrightness - x)
-                strip.show
-                print(str(currrentBrightness - x))
-                time.sleep(.030)
-
-        #strip.setBrightness(int(msg.payload))
-        #strip.show
+        fade_brightness(msg.payload)
+        
         bright = np.uint8(strip.getBrightness())
         print("before: " + str(currrentBrightness) + " / msg:: " + str(msg.payload) + " / actual: " + str(bright))
+
+    elif msg.topic == "zimmer/map/light/switch":
+        if msg.payload == "ON" and np.uint8(strip.getBrightness()) == 0:
+
     else:
         print("else")
     
     print(" ")
     strip.setPixelColorRGB(1, 0, 255, 0)
     strip.show()
+
+def fade_brightness(value):
+    dif = int(msg.payload) - currrentBrightness
+    if dif > 0:
+        for x in range(1,dif+1):
+            strip.setBrightness(currrentBrightness + x)
+            strip.show
+            print(str(currrentBrightness + x))
+            time.sleep(.030)
+    elif dif < 0:
+        dif = dif * (-1)
+        for x in range(1,dif+1):
+            strip.setBrightness(currrentBrightness - x)
+            strip.show
+            print(str(currrentBrightness - x))
+            time.sleep(.030)
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
