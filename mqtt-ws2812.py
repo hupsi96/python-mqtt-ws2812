bcea584881cf36bb66f3b05928063efc3e831a7f
Christian Huppertz <christian.huppertz@tum.de>
2018-08-09 23:34:38 +0200

    test

--- mqtt-ws2812.py
+++ mqtt-ws2812.py
@@ -31,6 +31,8 @@ def on_message(client, userdata, msg):
         print(str(np.uint8(msg.payload)))
         strip.setBrightness(int(np.uint8(msg.payload)))
         strip.show()
+        print("aktuelle Helligkeit")
+        print(str(np.uint8(strip.getBrightness)))
         #fade_brightness(msg.payload,.010)
 #Switch        
     #elif msg.topic == "zimmer/map/light/switch":
