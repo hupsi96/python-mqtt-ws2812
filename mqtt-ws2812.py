3c5f5a467c1ed64df518fb60a2ae94f17402456b
Christian Huppertz <christian.huppertz@tum.de>
2018-08-09 22:18:27 +0200

    test

--- mqtt-ws2812.py
+++ mqtt-ws2812.py
@@ -19,12 +19,12 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
-    strip.setPixelColorRGB(5,0,0,0)
-    strip.setPixelColorRGB(4,0,0,0)
-    strip.setPixelColorRGB(3,0,0,0)
-    strip.setPixelColorRGB(2,0,0,0)
-    strip.setPixelColorRGB(1,0,0,0)
-    strip.setPixelColorRGB(0,0,0,0)
+    strip.setPixelColorRGB(5,0,0,1)
+    strip.setPixelColorRGB(4,0,0,2)
+    strip.setPixelColorRGB(3,0,0,3)
+    strip.setPixelColorRGB(2,0,0,4)
+    strip.setPixelColorRGB(1,0,0,5)
+    strip.setPixelColorRGB(0,0,0,6)
     strip.show
     global currrentBrightness
     global stateoff
