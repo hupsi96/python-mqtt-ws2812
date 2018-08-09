f7638e0676e10366bedbf84d2adb31d43b4b8ca1
Christian Huppertz <christian.huppertz@tum.de>
2018-08-09 17:09:30 +0200

    test

--- mqtt-ws2812.py
+++ mqtt-ws2812.py
@@ -82,55 +82,58 @@ def fade_color(red,green,blue,fadeTime):
         value = value.rstrip('L')
         lv = len(value)
         rgbCurrentColor = tuple(int(value[i:i + lv // 3], 16) for i in range(0,lv, lv // 3))
-        if (int(rgbCurrentColor[0]) - int(red)) >= 0:
-            reddif = int(rgbCurrentColor[0]) - int(red)
-        else:
-            reddif = (int(rgbCurrentColor[0]) - int(red)) * (-1)
-        if (int(rgbCurrentColor[1]) - int(green)) >= 0:
-            greendif = int(rgbCurrentColor[1]) - int(green)
-        else:
-            greendif = (int(rgbCurrentColor[1]) - int(green)) * (-1)
-        if (int(rgbCurrentColor[2]) - int(blue)) >= 0:
-            bluedif = int(rgbCurrentColor[2]) - int(blue)
-        else:
-            bluedif = (int(rgbCurrentColor[2]) - int(blue)) * (-1)
-        if reddif > dif:
-            dif = reddif
-        if greendif > dif:
-            dif = greendif
-        if bluedif > dif:
-            dif = bluedif
-    for x in range(1,dif+1):
-        for pixel in range(strip.numPixel()):
-            currentColor = hex(np.asscalar(np.uint32(strip.getPixelColor(x))))
-            value = currentColor.lstrip('0x')
-            value = value.rstrip('L')
-            lv = len(value)
-            rgbCurrentColor = tuple(int(value[i:i + lv // 3], 16) for i in range(0,lv, lv // 3))
-            newRedValue = 0
-            newGreenValue = 0
-            newBlueValue = 0
-            if red > rgbCurrentColor[0]:
-                newRedValue = rgbCurrentColor[0] + 1
-            elif red < rgbCurrentColor[0]:
-                newRedValue = rgbCurrentColor[0] - 1
+        if len(rgbCurrentColor) > 3:
+            if (int(rgbCurrentColor[0]) - int(red)) >= 0:
+                reddif = int(rgbCurrentColor[0]) - int(red)
             else:
-                newRedValue = red
+                reddif = (int(rgbCurrentColor[0]) - int(red)) * (-1)
+            if (int(rgbCurrentColor[1]) - int(green)) >= 0:
+                greendif = int(rgbCurrentColor[1]) - int(green)
+            else:
+                greendif = (int(rgbCurrentColor[1]) - int(green)) * (-1)
+            if (int(rgbCurrentColor[2]) - int(blue)) >= 0:
+                bluedif = int(rgbCurrentColor[2]) - int(blue)
+            else:
+                bluedif = (int(rgbCurrentColor[2]) - int(blue)) * (-1)
+            if reddif > dif:
+                dif = reddif
+            if greendif > dif:
+                dif = greendif
+            if bluedif > dif:
+                dif = bluedif
 
-            if green > rgbCurrentColor[1]:
-                newGreenValue = rgbCurrentColor[1] + 1
-            elif green < rgbCurrentColor[1]:
-                newGreenValue = rgbCurrentColor[1] - 1
-            else:
-                newGreenValue = green
+        for x in range(1,dif+1):
+            for pixel in range(strip.numPixel()):
+                currentColor = hex(np.asscalar(np.uint32(strip.getPixelColor(x))))
+                value = currentColor.lstrip('0x')
+                value = value.rstrip('L')
+                lv = len(value)
+                rgbCurrentColor = tuple(int(value[i:i + lv // 3], 16) for i in range(0,lv, lv // 3))
+                newRedValue = 0
+                newGreenValue = 0
+                newBlueValue = 0
+                if len(rgbCurrentColor) > 3:
+                    if red > rgbCurrentColor[0]:
+                        newRedValue = rgbCurrentColor[0] + 1
+                    elif red < rgbCurrentColor[0]:
+                        newRedValue = rgbCurrentColor[0] - 1
+                    else:
+                        newRedValue = red
 
-            if blue > rgbCurrentColor[3]:
-                newBlueValue = rgbCurrentColor[3] + 1
-            elif blue < rgbCurrentColor[3]:
-                newBlueValue = rgbCurrentColor[3] - 1
-            else:
-                newBlueValue = blue
-            strip.setPixelColorRGB(pixel,newRedValue,newGreenValue,newBlueValue)
+                    if green > rgbCurrentColor[1]:
+                        newGreenValue = rgbCurrentColor[1] + 1
+                    elif green < rgbCurrentColor[1]:
+                        newGreenValue = rgbCurrentColor[1] - 1
+                    else:
+                        newGreenValue = green
+
+                    if blue > rgbCurrentColor[3]:
+                        newBlueValue = rgbCurrentColor[3] + 1
+                    elif blue < rgbCurrentColor[3]:
+                        newBlueValue = rgbCurrentColor[3] - 1
+                    else:
+                        newBlueValue = blue
+                    strip.setPixelColorRGB(pixel,newRedValue,newGreenValue,newBlueValue)
         strip.show
 
 
