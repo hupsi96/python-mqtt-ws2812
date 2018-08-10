708d746a7a5b4961052e85fd9fbd6753fd5b6429
Christian Huppertz <christian.huppertz@tum.de>
2018-08-10 10:27:10 +0200

    test

--- mqtt-ws2812.py
+++ mqtt-ws2812.py
@@ -48,7 +48,7 @@ def on_message(client, userdata, msg):
     #Brightness
     if msg.topic == "zimmer/map/brightness/set":
         #setStripBrightness(int(msg.payload))
-        fade_brightness(int(msg.payload))
+        fadeStripBrightness(int(msg.payload))
     #Switch        
     #elif msg.topic == "zimmer/map/light/switch":
         #if msg.payload == "ON" and stateoff == True:
