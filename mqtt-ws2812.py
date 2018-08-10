48307b9ba96b9e24899b67ff995d679e962801ec
Christian Huppertz <christian.huppertz@tum.de>
2018-08-10 14:32:52 +0200

    test

--- mqtt-ws2812.py
+++ mqtt-ws2812.py
@@ -75,7 +75,7 @@ def fadeStripBrightness(value,speed):
     for x in range(strip.numPixels()):
         for y in range(3):
             #print(str(matrix[x][y]) +"-"+str(matrix[x][y+3]) +"*"+ str(value) +"/" + str(itterations))
-            matrix[x][y] = (((matrix[x][y]-(matrix[x][y+3] * value)) / itterations)
+            matrix[x][y] = ((matrix[x][y]-(matrix[x][y+3] * value)) / itterations)
             print(matrix[x][y])
         print("")
 
