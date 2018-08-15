5a95e13bbabab8589e5f00dad2eabe87e52a0879
Christian Huppertz <christian.huppertz@tum.de>
2018-08-15 21:31:25 +0200

    test api call

--- mqtt-ws2812.py
+++ mqtt-ws2812.py
@@ -6,8 +6,12 @@ import time
 from random import randint
 import requests
 
+myToken = 'b08f4d8e5f3a74c2e1417c081ef20823'
+head = {'Authorization': 'token {}'.format(myToken)}
+myURL = 'http://samples.openweathermap.org/data/2.5/weather?q=London,uk'
+response = requests.get(myUrl, headers=head)
 
-
+print(str(response.status_code))
 
 stateoff = False
 strip = Adafruit_NeoPixel(100, 18, 800000, 5, False, 255)
