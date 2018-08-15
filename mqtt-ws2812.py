83a93072383ba19d5188eba760a60f6b7355eb65
Christian Huppertz <christian.huppertz@tum.de>
2018-08-15 11:06:27 +0200

    syntax error fixing

--- mqtt-ws2812.py
+++ mqtt-ws2812.py
@@ -10,7 +10,8 @@ from random import randint
 stateoff = True
 strip = Adafruit_NeoPixel(20, 18, 800000, 5, False, 255)
 strip.begin()
-global defaultColor = (255,255,255)
+global defaultColor 
+defaultColor = (255,255,255)
 
 def clear():
     for x in range(strip.numPixels()):
