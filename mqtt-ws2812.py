debd17ee0d7127c46c16dd7f6d72292f8fda8d81
Christian Huppertz <christian.huppertz@tum.de>
2018-08-17 11:03:23 +0200

    remove commented and dead code

--- mqtt-ws2812.py
+++ mqtt-ws2812.py
@@ -152,6 +152,7 @@ def weatherMap():
     for x in range(len(cityList)):
         myUrl = 'http://api.openweathermap.org/data/2.5/weather?id=' + cityList[x][0] + myToken
         print(myUrl)
+        #errors have to be caught here 
         response = requests.get(myUrl)
         print(str(response.status_code))
         output = json.loads(response.text)
@@ -270,90 +271,3 @@ def startMQTT():
 
 p1 = multiprocessing.Process(target=startMQTT)
 p1.start()
-
-#thread.start_new_thread(startMQTT,())
-#mainThread = threading.Thread(target = startMQTT)
-#mainThread.start()
-#old code:
-
-# def fade_brightness(value,speed):
-#     curBright = currrentBrightness
-#     dif = int(value) - int(curBright)
-#     if dif > 0:
-#         for x in range(1,dif+1):
-#             strip.setBrightness(curBright + x)
-#             strip.show()
-#             print(str(curBright + x))
-#             time.sleep(speed)
-#     elif dif < 0:
-#         dif = dif * (-1)
-#         for x in range(1,dif+1):
-#             strip.setBrightness(curBright - x)
-#             strip.show()
-#             print(str(curBright - x))
-#             time.sleep(speed)
-#     if value != 0:
-#         global currrentBrightness
-#         currrentBrightness = np.uint8(strip.getBrightness())
-
-#def fade_color(red,green,blue,fadeTime):
-    #dif = 0
-    #for x in range(strip.numPixels()):
-        #currentColor = hex(np.asscalar(np.uint32(strip.getPixelColor(x))))
-        #value = currentColor.lstrip('0x')
-        #value = value.rstrip('L')
-        #lv = len(value)
-        #rgbCurrentColor = tuple(int(value[i:i + lv // 3], 16) for i in range(0,lv, lv // 3))
-        #if len(rgbCurrentColor) > 3:
-            #if (int(rgbCurrentColor[0]) - int(red)) >= 0:
-                #reddif = int(rgbCurrentColor[0]) - int(red)
-            #else:
-                #reddif = (int(rgbCurrentColor[0]) - int(red)) * (-1)
-            #if (int(rgbCurrentColor[1]) - int(green)) >= 0:
-                #greendif = int(rgbCurrentColor[1]) - int(green)
-            #else:
-                #greendif = (int(rgbCurrentColor[1]) - int(green)) * (-1)
-            #if (int(rgbCurrentColor[2]) - int(blue)) >= 0:
-                #bluedif = int(rgbCurrentColor[2]) - int(blue)
-            #else:
-                #bluedif = (int(rgbCurrentColor[2]) - int(blue)) * (-1)
-            #if reddif > dif:
-                #dif = reddif
-            #if greendif > dif:
-                #dif = greendif
-            #if bluedif > dif:
-                #dif = bluedif
-
-        #for x in range(1,dif+1):
-            #for pixel in range(strip.numPixel()):
-                #currentColor = hex(np.asscalar(np.uint32(strip.getPixelColor(x))))
-                #value = currentColor.lstrip('0x')
-                #value = value.rstrip('L')
-                #lv = len(value)
-                #rgbCurrentColor = tuple(int(value[i:i + lv // 3], 16) for i in range(0,lv, lv // 3))
-                #newRedValue = 0
-                #newGreenValue = 0
-                #newBlueValue = 0
-                #if len(rgbCurrentColor) > 3:
-                    #if red > rgbCurrentColor[0]:
-                        #newRedValue = rgbCurrentColor[0] + 1
-                    #elif red < rgbCurrentColor[0]:
-                        #newRedValue = rgbCurrentColor[0] - 1
-                    #else:
-                        #newRedValue = red
-
-                    #if green > rgbCurrentColor[1]:
-                        #newGreenValue = rgbCurrentColor[1] + 1
-                    #elif green < rgbCurrentColor[1]:
-                        #newGreenValue = rgbCurrentColor[1] - 1
-                    #else:
-                        #newGreenValue = green
-
-                    #if blue > rgbCurrentColor[3]:
-                        #newBlueValue = rgbCurrentColor[3] + 1
-                    #elif blue < rgbCurrentColor[3]:
-                        #newBlueValue = rgbCurrentColor[3] - 1
-                    #else:
-                        #newBlueValue = blue
-                    #strip.setPixelColorRGB(pixel,newRedValue,newGreenValue,newBlueValue)
-        #strip.show
