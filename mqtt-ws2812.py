ff483d9bdf975935dec8ed8f4fec1077463de5df
Christian Huppertz <christian.huppertz@tum.de>
2018-08-23 15:14:20 +0200

    test 165

--- mqtt-ws2812.py
+++ mqtt-ws2812.py
@@ -11,7 +11,7 @@ import json
 import logging
 
 stateoff = False
-strip = Adafruit_NeoPixel(135, 18, 800000, 5, False, 255) #165
+strip = Adafruit_NeoPixel(165, 18, 800000, 5, False, 255) #165
 strip.begin()
 defaultColor = (255,255,255)
 fadeTime = 1000
