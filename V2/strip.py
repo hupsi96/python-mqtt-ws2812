1b89671d2d5fb0c904ca5116ac1ddd970d5252c4
Christian Huppertz <christian.huppertz@tum.de>
2019-04-22 12:23:10 +0200

    reconnected

--- V2/strip.py
+++ V2/strip.py
@@ -1,5 +1,5 @@
 from neopixel import *
-#import logging
+import logging
 
 class strip_config:
     global strip
@@ -9,15 +9,11 @@ class strip_config:
         strip.begin()
 
     def clear(self):
-        #logging.basicConfig(filename='WS2812Controller.log', filemode='w', level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%d.%m.%y %I:%M:%S %p')
-        #logging.info('Strip cleared')
+        logging.basicConfig(filename='WS2812Controller.log', filemode='w', level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%d.%m.%y %I:%M:%S %p')
+        logging.info('Strip cleared')
         for x in range(strip.numPixels()):
             strip.setPixelColorRGB(x,0,0,0)
         strip.show()
 
     def start(self):
         strip.begin()
-
-#test = strip_config(177,18)
-#test.start()
-#test.clear()
