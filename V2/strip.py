7d3208f948562dc39edbf735ae160da33353f905
Christian Huppertz <christian.huppertz@tum.de>
2019-04-22 20:26:13 +0200

    testing

--- V2/strip.py
+++ V2/strip.py
@@ -36,7 +36,7 @@ class strip_config:
 
     #Sets the brightness of the whole strip
     def setStripBrightness(self,value):
-        print(hex(np.asscalar(np.uint32(strip.getBrightness))))
+        print(np.uint32(strip.getBrightness))
         strip.setBrightness(value)
         strip.show()
 
