061e16a0028a256dd95afcb4840a23af931cac3f
Christian Huppertz <christian.huppertz@tum.de>
2019-04-22 20:21:17 +0200

    test sleep time

--- V2/strip.py
+++ V2/strip.py
@@ -31,7 +31,7 @@ class strip_config:
         logging.info('Strip cleared')
         for x in range(strip.numPixels()):
             strip.setPixelColorRGB(x,0,0,0)
-            time.sleep(1)
+            time.sleep(100)
         strip.show()
 
     #Sets the brightness of the whole strip
