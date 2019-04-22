68dfbc4a8b67c96aa67a7c8526bcac144981badd
Christian Huppertz <christian.huppertz@tum.de>
2019-04-22 12:34:22 +0200

    testing

--- V2/strip.py
+++ V2/strip.py
@@ -2,18 +2,17 @@ from neopixel import *
 import logging
 
 class strip_config:
+
     global strip
     strip = Adafruit_NeoPixel(177, 18, 800000, 5, False, 255) #default setting
+    logging.basicConfig(filename='WS2812Controller.log', filemode='w', level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%d.%m.%y %I:%M:%S %p')
+
     def __init__(self, num, pin):
         self.strip = Adafruit_NeoPixel(num,pin,800000,10,False,255)
         strip.begin()
 
     def clear(self):
-        logging.basicConfig(filename='WS2812Controller.log', filemode='w', level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%d.%m.%y %I:%M:%S %p')
         logging.info('Strip cleared')
         for x in range(strip.numPixels()):
             strip.setPixelColorRGB(x,0,0,0)
         strip.show()
-
-    def start(self):
-        strip.begin()
