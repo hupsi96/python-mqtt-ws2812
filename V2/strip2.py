bedb1d5882799dd9c8b2d4a8c60b00b0869b9d6b
Christian Huppertz <christian.huppertz@tum.de>
2019-04-22 13:57:54 +0200

    test2

--- V2/strip2.py
+++ V2/strip2.py
@@ -2,9 +2,9 @@ import time
 import board
 import neopixel
 
-pixel_pin = board.Board.D18
+#pixel_pin = board.D18
 num_pixels = 30
 ORDER = neopixel.RGB
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False,pixel_order=ORDER)
+pixels = neopixel.NeoPixel(board.D18, num_pixels, brightness=0.2, auto_write=False,pixel_order=ORDER)
 
