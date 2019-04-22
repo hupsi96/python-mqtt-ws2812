22fe7c29b69ca610873e719c2410f0041c2dc06a
Christian Huppertz <christian.huppertz@tum.de>
2019-04-22 11:34:09 +0200

    edjust strip confic

--- V2/strip.py
+++ V2/strip.py
@@ -1,7 +1,7 @@
 from neopixel import *
 
 class strip:
-    global strip  # = Adafruit_NeoPixel(177, 18, 800000, 5, False, 255) #Pin = 18 (2nd Param)
+    strip = Adafruit_NeoPixel(177, 18, 800000, 5, False, 255) #Pin = 18 (2nd Param)
     def __init__(self, num, pin):
         strip = Adafruit_NeoPixel(num,pin,800000,10,False,255)
         strip.begin()
