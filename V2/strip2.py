03572543d1e66e51d9b0de3dc93b0aafbaa76544
Christian Huppertz <christian.huppertz@tum.de>
2019-04-22 13:59:11 +0200

    test3

--- V2/strip2.py
+++ V2/strip2.py
@@ -4,7 +4,7 @@ import neopixel
 
 #pixel_pin = board.D18
 num_pixels = 30
-ORDER = neopixel.RGB
+#ORDER = neopixel.RGB
 
-pixels = neopixel.NeoPixel(board.D18, num_pixels, brightness=0.2, auto_write=False,pixel_order=ORDER)
+pixels = neopixel.NeoPixel(board.D18, num_pixels, brightness=0.2, auto_write=False,pixel_order=neopixel.RGB)
 
