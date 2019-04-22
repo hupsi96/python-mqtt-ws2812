4be34c4829cc3c92be582bf2911f224f2d19ca07
Christian Huppertz <christian.huppertz@tum.de>
2019-04-22 11:27:13 +0200

    test3

--- V2/mqtt.py
+++ V2/mqtt.py
@@ -17,7 +17,7 @@ class main:
         strip = st.strip(177, 18)
         # Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
-        #client.subscribe("$SYS/#")
+        client.subscribe("zimmer/#")
 
     # The callback for when a PUBLISH message is received from the server.
     def on_message(client, userdata, msg):
