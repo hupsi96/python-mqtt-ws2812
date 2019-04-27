c8fb401dba8bd6629057ff185c56261c0c742ddd
Christian Huppertz <christian.huppertz@tum.de>
2019-04-27 19:08:22 +0200

    testing to splint into new files

---
+++ V2/map_animation.py
@@ -0,0 +1,20 @@
+from neopixel import *
+import logging
+import time
+
+class animation_setup:
+
+    global waveArray
+    global strip
+
+    def __init__(self,stripInput):
+
+        self.strip = self.stripInput
+        self.waveArray = [[103],[102,104],[101,105,106],[100,107,108,109],[57,99,110,111]]
+
+    def waveAnimation(self):
+        for x in self.waveAnimation:
+            for y in x:
+                strip.setBrightness(100)
+            strip.show()
+            time.sleep(5)
--- V2/strip.py
+++ V2/strip.py
@@ -2,12 +2,14 @@ from neopixel import *
 import logging
 import time
 import numpy as np
-
+import map_animation as animat
 class strip_config:
 
     #define strip globaly and set default values
     global strip
     strip = Adafruit_NeoPixel(177, 18, 800000, 5, False, 255) #default setting """Adafruit_NeoPixel"""
+
+    global animationClass = animat.animation_setup(strip)
 
     #confic logging module
     logging.basicConfig(filename='WS2812Controller.log', filemode='w', level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%d.%m.%y %I:%M:%S %p')
